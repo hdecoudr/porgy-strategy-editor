eed4df813a945f565e992b81dbce6d816e683eb0
unknown <hdecoudras@gmail.com>
2021-09-06 16:10:45 +0200

    xmlsyntaxparser

---
+++ xmlsyntaxparser.cpp
@@ -0,0 +1,205 @@
+/**
+ *
+ * This file is part of Porgy (http://tulip.labri.fr/TulipDrupal/?q=porgy)
+ *
+ * from LaBRI, University of Bordeaux, Inria and King's College London
+ *
+ * Porgy is free software;  you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Porgy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this Porgy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "xmlsyntaxparser.h"
+#include "highlightingrule.h"
+#include "generalpurposesyntaxhighlightingrules.h"
+
+#include <QFile>
+#include <QXmlStreamReader>
+
+bool XmlSyntaxParser::parse(const QString& file, GeneralPurposeSyntaxHighlightingRules* rules, QString* error)
+{
+    QFile f(file);
+
+    if(f.open(QIODevice::ReadOnly))
+    {
+        QXmlStreamReader reader(&f);
+
+        QXmlStreamAttributes        attributes;
+        QXmlStreamReader::TokenType token;
+        QString                     element;
+
+        QTextCharFormat             cFormat;
+
+        while(!reader.atEnd())
+        {
+            token = reader.readNext();
+
+            if(reader.hasError())
+            {
+                if(error)
+                {
+                    *error = reader.errorString();
+                }
+
+                return false;
+            }
+
+            if(token == QXmlStreamReader::StartDocument)
+            {
+                continue;
+            }
+
+            if(token == QXmlStreamReader::StartElement)
+            {
+                if(reader.name() == "name")
+                {
+                    //
+                    // Do something with name.
+                    //
+                }
+                else if(reader.name() == "attributes")
+                {
+                    while(!reader.atEnd())
+                    {
+                        token = reader.readNext();
+
+                        if(reader.hasError())
+                        {
+                            if(error)
+                            {
+                                *error = reader.errorString();
+                            }
+
+                            return false;
+                        }
+
+                        if(token == QXmlStreamReader::StartElement)
+                        {
+
+                            if(reader.name() == "instructions")
+                            {
+                                attributes = reader.attributes();
+                                cFormat.setForeground(XmlSyntaxParser::intToRgb(attributes.at(0).value().toString().toInt()));
+                                cFormat.setFontWeight(XmlSyntaxParser::isBold(attributes.at(1).value().toString()));
+                                cFormat.setFontItalic(XmlSyntaxParser::isItalic(attributes.at(1).value().toString()));
+                                element = "instruction";
+                            }
+                            else if(reader.name() == "functions")
+                            {
+                                attributes = reader.attributes();
+                                cFormat.setForeground(XmlSyntaxParser::intToRgb(attributes.at(0).value().toString().toInt()));
+                                cFormat.setFontWeight(XmlSyntaxParser::isBold(attributes.at(1).value().toString()));
+                                cFormat.setFontItalic(XmlSyntaxParser::isItalic(attributes.at(1).value().toString()));
+                                element = "function";
+                            }
+                            else if(reader.name() == "operators")
+                            {
+                                attributes = reader.attributes();
+                                cFormat.setForeground(XmlSyntaxParser::intToRgb(attributes.at(0).value().toString().toInt()));
+                                cFormat.setFontWeight(XmlSyntaxParser::isBold(attributes.at(1).value().toString()));
+                                cFormat.setFontItalic(XmlSyntaxParser::isItalic(attributes.at(1).value().toString()));
+                                element = "operator";
+                            }
+                            else if(reader.name() == "symbols")
+                            {
+                                attributes = reader.attributes();
+                                cFormat.setForeground(XmlSyntaxParser::intToRgb(attributes.at(0).value().toString().toInt()));
+                                cFormat.setFontWeight(XmlSyntaxParser::isBold(attributes.at(1).value().toString()));
+                                cFormat.setFontItalic(XmlSyntaxParser::isItalic(attributes.at(1).value().toString()));
+                                element = "symbol";
+                            }
+                            else if(reader.name() == "singlecomments")
+                            {
+                                attributes = reader.attributes();
+                                cFormat.setForeground(XmlSyntaxParser::intToRgb(attributes.at(0).value().toString().toInt()));
+                                cFormat.setFontWeight(XmlSyntaxParser::isBold(attributes.at(1).value().toString()));
+                                cFormat.setFontItalic(XmlSyntaxParser::isItalic(attributes.at(1).value().toString()));
+                                element = "singlecomment";
+                            }
+                            else if(reader.name() == "multicomments")
+                            {
+                                attributes = reader.attributes();
+                                cFormat.setForeground(XmlSyntaxParser::intToRgb(attributes.at(0).value().toString().toInt()));
+                                cFormat.setFontWeight(XmlSyntaxParser::isBold(attributes.at(1).value().toString()));
+                                cFormat.setFontItalic(XmlSyntaxParser::isItalic(attributes.at(1).value().toString()));
+                                element = "multicomment";
+                            }
+                            else if(reader.name() == "quotations")
+                            {
+                                attributes = reader.attributes();
+                                cFormat.setForeground(XmlSyntaxParser::intToRgb(attributes.at(0).value().toString().toInt()));
+                                cFormat.setFontWeight(XmlSyntaxParser::isBold(attributes.at(1).value().toString()));
+                                cFormat.setFontItalic(XmlSyntaxParser::isItalic(attributes.at(1).value().toString()));
+                                element = "quotation";
+                            }
+                            else
+                            {
+                                if(error)
+                                {
+                                    *error = "Unnown XML element: " + reader.name().toString();
+                                }
+
+                                return false;
+                            }
+
+                            while(!reader.atEnd())
+                            {
+                                token = reader.readNext();
+
+                                if(reader.hasError())
+                                {
+                                    if(error)
+                                    {
+                                        *error = reader.errorString();
+                                    }
+
+                                    return false;
+                                }
+
+                                if(token == QXmlStreamReader::StartElement && reader.name() == element)
+                                {
+                                    if(element == "function")
+                                    {
+                                        QRegExp rExp(reader.attributes().at(0).value().toString());
+                                        HighlightingRule* hRule = new HighlightingRule(rExp, cFormat);
+                                        rules->appendRule(element.toUtf8().constData(), hRule);
+                                    }
+                                    else
+                                    {
+                                        QRegExp rExp(reader.readElementText());
+                                        HighlightingRule* hRule = new HighlightingRule(rExp, cFormat);
+                                        rules->appendRule(element.toUtf8().constData(), hRule);
+                                    }
+                                }
+                                else
+                                {
+                                    if(token == QXmlStreamReader::EndElement && reader.name() != element)
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        *error = "Unable to open file : " + file + "\n" + f.errorString();
+        return false;
+    }
+
+    f.close();
+    return true;
+}
---
+++ xmlsyntaxparser.h
@@ -0,0 +1,73 @@
+﻿/**
+ *
+ * This file is part of Porgy (http://tulip.labri.fr/TulipDrupal/?q=porgy)
+ *
+ * from LaBRI, University of Bordeaux, Inria and King's College London
+ *
+ * Porgy is free software;  you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Porgy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this Porgy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef XMLSYNTAXPARSER_H
+#define XMLSYNTAXPARSER_H
+
+#include <QBrush>
+#include <QFont>
+#include <QXmlStreamReader>
+
+class GeneralPurposeSyntaxHighlightingRules;
+
+class XmlSyntaxParser
+{
+public:
+    static bool parse(const QString& file, GeneralPurposeSyntaxHighlightingRules* rules, QString* error = 0);
+
+public:
+    inline static QBrush intToRgb(int color)
+    {
+        return QBrush(QColor((color >> 0x10) & 0xFF, (color >> 0x08) & 0xFF, color & 0xFF));
+    }
+
+    inline static int isBold(const QString& style)
+    {
+        if(style.contains("bold"))
+        {
+            return QFont::Bold;
+        }
+
+        return QFont::Normal;
+    }
+
+    inline static bool isItalic(const QString& style)
+    {
+        return (style.contains("italic"));
+    }
+
+    inline static bool hasWordBoundary(const QString& word)
+    {
+        return (word.contains("\\b"));
+    }
+
+    inline static bool removeWordBoundary(QString& word)
+    {
+        if(XmlSyntaxParser::hasWordBoundary(word))
+        {
+            word = word.mid(2, word.size() - 4);
+            return true;
+        }
+
+        return false;
+    }
+};
+
+#endif // XMLSYNTAXPARSER_H
